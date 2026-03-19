766bc1515ea6d6227808a11a28ee7d78d9856fba
metal-chris <121712571+metal-chris@users.noreply.github.com>
2026-03-19 18:09:26 -0500

    feat: add personality and wit to About page
    - Added self-aware humor ('Another person with Opinions™')
    - Made hot takes more confident and playful
    - Added Spider-Man quote twist at the end
    - Separated hot takes into own paragraph for emphasis
    - More conversational tone throughout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,19 +24,22 @@ export default function AboutPage() {
           That&rsquo;s what pop culture has always been for me: a way to connect. A language we speak when regular words don&rsquo;t cut it.
         </p>
         <p>
-          When life got heavy&mdash;and <em>it did</em>&mdash;I found myself often turning to Spider-Man comics. Not because they had all the answers, but because Peter Parker never gave up. No matter how bad things got, no matter how much the world pushed back, he kept swinging. That resilience became something I needed to see, over and over again.
+          When life got heavy&mdash;and <em>it did</em>&mdash;I found myself turning to Spider-Man comics. Not because they had all the answers, but because Peter Parker never gave up. No matter how bad things got, no matter how much the world pushed back, he kept swinging. That resilience became something I needed to see, over and over again.
         </p>
         <p>
-          <strong className="text-foreground">The Spidaverse</strong> is my way of paying that forward. It&rsquo;s where I share the movies, games, anime, manga, and music that got me through&mdash;and continue to get me through. Every piece of media gets a <strong className="text-foreground">Web Rating</strong> out of 100, visualized as a spider web that fills based on the score. Not because I want to debate whether something deserves an 87 or a 91, but because sometimes you just need to quantify how much something mattered to you. The emphasis being that these things connect us to one another.
+          <strong className="text-foreground">The Spidaverse</strong> is my way of paying that forward. It&rsquo;s where I share the movies, games, anime, manga, and music that got me through&mdash;and continue to get me through. Every piece of media gets a <strong className="text-foreground">Web Rating</strong> out of 100, visualized as a spider web that fills based on the score. Not because I want to debate whether something deserves an 87 or a 91, but because sometimes you just need to quantify how much something mattered to you.
         </p>
         <p>
-          I&rsquo;m not here to prove anything right or wrong. I&rsquo;m not here to win arguments in the comments (the internet&rsquo;s already got enough of that). I&rsquo;m just here to nerd out with you about the stuff we love. To share why a &ldquo;bad&rdquo; movie hit different for me. To put you on to that J-Pop/Rap artist you&rsquo;ve been sleeping on. To defend <em>Batman v Superman</em> against all odds (yes, really).
+          Look, I know what you&rsquo;re thinking: <em>&ldquo;Another person with Opinions™ about movies on the internet.&rdquo;</em> Fair. But I&rsquo;m not here to prove I&rsquo;m right or win arguments in the comments (the internet&rsquo;s already got enough of that). I&rsquo;m just here to nerd out with you about the stuff we love. To share why a &ldquo;bad&rdquo; movie hit different for me. To put you on to that J-Pop/Rap artist you&rsquo;ve been sleeping on. To defend <em>Batman v Superman</em> like my life depends on it (yes, really&mdash;and no, I will not be taking questions at this time).
+        </p>
+        <p>
+          Some hills I&rsquo;ll die on: J-Pop/Rap is criminally underrated. Pineapple does not belong on pizza. <em>Batman v Superman</em> got way more hate than it deserved. And if you disagree? Cool. That&rsquo;s what makes this fun.
         </p>
         <p>
           Spider-Man inspires hope. I&rsquo;m not him&mdash;I&rsquo;m just trying to be someone he&rsquo;d respect while being my own person. Because anyone can wear the mask. Anyone can be Spider-Man. And we&rsquo;re all in this together, connected by the stories that shape us.
         </p>
         <p className="text-muted-foreground text-sm border-l-2 border-border pl-4">
-          Welcome to the web!
+          With great power comes great responsibility to have correct opinions about anime. Welcome to the web.
         </p>
       </div>
     </Container>
